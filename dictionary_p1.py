f7612c4750b5e2e2e2106105f8fdeffde6dc32b2
Unixprime7 <michaeld801@nycstudents.net>
2026-02-12 09:24:20 -0500

    Create dictionary_p1.py

---
+++ dictionary_p1.py
@@ -0,0 +1,27 @@
+martwal = [
+
+    {
+    "name": "Pikachu-stuffed toy/plushie",
+    "price": 3,
+    "size": '7"',
+    "description": "If you don't buy me, I will find you",
+    }
+    {
+    "name": "Rock/Large-Pebble/Tiny-Boulder",
+    "price": 5,
+    "size": "Rock",
+    "description": "Rock",
+    }
+    {
+    "name": "N/A",
+    "price": 99999,
+    "size": "N/A",
+    "description": "pls speed I need this",
+    }
+]
+
+
+print("Product options:  Pikachu, Rock, Actual_Product_Trust")
+buying = input("What would you like to buy?")
+if buying == "Pikachu":
+    martwal[0]["name"]
