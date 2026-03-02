77fb2337c00adb2ee6d23d5d2cf102b29682f68b
Unixprime7 <michaeld801@nycstudents.net>
2026-03-02 09:12:40 -0500

    Update dictionary_p2.py

--- dictionary_p2.py
+++ dictionary_p2.py
@@ -21,15 +21,24 @@ martwal = [
 
 
 print("Product options:  Pikachu, Rock, Actual_Product_Trust")
-buying = input("What would you like to buy? ")
+
 still_buying = 1
+cart = []
+
 while still_buying == 1:
-    
+    buying = input("What would you like to buy? ")
+    if buying == "Pikachu":
+     print(martwal[0]["name"])
+    elif buying == "Rock":
+     print(martwal[1]["name"])
+    elif buying == "Actual_Product_Trust":
+     print(martwal[2]["name"])
 
+    cart.append(buying)
 
-# if buying == "Pikachu":
-#     print(martwal[0]["name"])
-# elif buying == "Rock":
-#     print(martwal[1]["name"])
-# elif buying == "Actual_Product_Trust":
-#     print(martwal[2]["name"])
+    keep_shopping = input("Would you like to buy something else? ")
+    if keep_shopping != "yes":
+      still_buying = 0
+
+print("imagine buying", *cart)
+print("u just got scammed at martwal ez")
