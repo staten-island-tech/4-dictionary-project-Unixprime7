dff2058aeb510a876c1fb3a47faacc4be4fed0d7
Unixprime7 <michaeld801@nycstudents.net>
2026-04-13 09:26:33 -0400

    Update dictionary_p2.py

--- dictionary_p2.py
+++ dictionary_p2.py
@@ -1,18 +1,18 @@
 martwal = [
     {
-    "name": "Pikachu-stuffed toy/plushie",
+    "name": "Pikachu",
     "price": 3,
     "size": '7"',
     "description": "If you don't buy me, I will find you",
     },
     {
-    "name": "Rock/Large-Pebble/Tiny-Boulder",
+    "name": "Rock",
     "price": 5,
     "size": "Rock",
     "description": "Rock",
     },
     {
-    "name": "N/A",
+    "name": "Actual_Product_Trust",
     "price": 99999,
     "size": "N/A",
     "description": "pls speed I need this",
@@ -27,14 +27,10 @@ cart = []
 
 while still_buying == 1:
     buying = input("What would you like to buy? ")
-    if buying == "Pikachu":
-     print(martwal[0]["name"])
-    elif buying == "Rock":
-     print(martwal[1]["name"])
-    elif buying == "Actual_Product_Trust":
-     print(martwal[2]["name"])
-
-    cart.append(buying)
+    for product in martwal:
+       if buying == product['name']:
+          print("You just purchased", buying)
+          cart.append(buying)
 
     keep_shopping = input("Would you like to buy something else? ")
     if keep_shopping != "yes":
